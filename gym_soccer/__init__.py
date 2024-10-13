c3e10a7e43aafaa261b2843378cc61722259373a
Miguel Morales <mimoralea@gmail.com>
2024-10-13 10:17:40 -0600

    No registration (gym is not MA, pettingzoo has different API)

--- gym_soccer/__init__.py
+++ gym_soccer/__init__.py
@@ -1,12 +1,12 @@
 from gym.envs.registration import register
 
-
+# NO REGISTRATION JUST YET
 # classics
-register(
-    id='Soccer5x4Simultaneous-v0',
-    entry_point='gym_soccer.envs:SoccerEnv',
-    kwargs={'width': 5, 'height': 4, 'slip_prob': 0.2, 'isd_possession_a': 0.5, 'player_a_policy': None, 'player_b_policy': None},
-    max_episode_steps=100,
-    reward_threshold=1.0,
-    nondeterministic=True,
-)
+# register(
+#     id='SoccerSimultaneous-v0',
+#     entry_point='gym_soccer.envs:SoccerSimultaneousEnv',
+#     kwargs={'width': 5, 'height': 4, 'slip_prob': 0.2, 'player_a_policy': None, 'player_b_policy': None},
+#     max_episode_steps=100,
+#     reward_threshold=1.0,
+#     nondeterministic=True,
+# )
